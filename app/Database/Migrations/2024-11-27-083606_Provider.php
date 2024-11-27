872a10a3c9511d77eebe869a75fd223c66ab74c8
MelinaGrgt <frb17@hotmail.fr>
2024-11-27 09:36:43 +0100

    feat: replaced template migrations with project

---
+++ app/Database/Migrations/2024-11-27-083606_Provider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Provider extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
