51e0e14b32f34854e152602af2397e567594f5fd
MelinaGrgt <frb17@hotmail.fr>
2024-11-27 11:04:00 +0100

    feat: completed upload of migration files

--- app/Database/Migrations/2024-11-27-083606_Provider.php
+++ app/Database/Migrations/2024-11-27-083606_Provider.php
@@ -8,11 +8,35 @@ class Provider extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+           'id' => [
+               'type' => 'INT',
+               'constraint' => 11,
+               'unsigned' => true,
+               'auto_increment' => true,
+           ],
+           'name' => [
+               'type' => 'VARCHAR',
+               'constraint' => 255,
+           ],
+           'address' => [
+               'type' => 'TEXT',
+           ],
+           'city_id' => [
+               'type' => 'INT',
+               'constraint' => 11,
+               'unsigned' => true,
+           ]
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('city_id', 'city', 'id', 'CASCADE', 'CASCADE');
+
+        $this->forge->createTable('provider');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('provider');
     }
 }
