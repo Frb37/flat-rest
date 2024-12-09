a6615b6d6ae0f2e1799d5dc4a7829d13499c8ea2
MelinaGrgt <frb17@hotmail.fr>
2024-12-09 11:41:50 +0100

    fix: fixed problems with foreign key error in comment tables

--- app/Database/Migrations/2024-12-09-092631_MealCategory.php
+++ app/Database/Migrations/2024-12-09-092631_MealCategory.php
@@ -8,11 +8,25 @@ class MealCategory extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'              => 'INT',
+                'constraint'        => 11,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'name' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('meal_category');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('meal_category');
     }
 }
