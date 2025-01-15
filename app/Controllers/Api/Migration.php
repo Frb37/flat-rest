5edec4841443c091f9d7906557dc2bfad3812c4f
MelinaGrgt <frb17@hotmail.fr>
2025-01-15 09:56:24 +0100

    fix: added migration via API in main.yml * filled in Item and Migration controllers for API

--- app/Controllers/Api/Migration.php
+++ app/Controllers/Api/Migration.php
@@ -2,7 +2,36 @@
 
 namespace App\Controllers\Api;
 
-class Migration
-{
+use CodeIgniter\Config\Services;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\RESTful\ResourceController;
 
+class Migration extends ResourceController
+{
+    /**
+     * Return an array of resource objects, themselves in array format.
+     *
+     * @return ResponseInterface
+     */
+    public function getindex()
+    {
+        // Retrieve header token
+        $token = $this->request->getHeaderLine('Authorization');
+        // Retrieve token as define in .env
+        $staticToken = getenv('ENV_TOKEN');
+        if ($token !== $staticToken) {
+            return $this->failUnauthorized('Invalid token');
+        }
+        $migrations = Services::migrations();
+        try {
+            // Execute all migrations
+            $migrations->latest();
+
+            // Return success with code 200
+            return $this->respond(['message' => "Migrations successful!"], 200);
+        } catch (\Exception $e) {
+            // Return error message
+            return $this->respond(['message' => "Migrations unsuccessful. An error occurred: " . $e->getMessage()], 500);
+        }
+    }
 }
